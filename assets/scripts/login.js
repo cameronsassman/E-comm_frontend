ffe3419719d8cb979b39cf6356635e50e49da0b8
Siyamthanda <ludongasiya@gmail.com>
2021-02-24 16:25:39 +0200

    lonely fans

---
+++ assets/scripts/login.js
@@ -0,0 +1,9 @@
+// Get the modal
+const login = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == login) {
+    login.style.display = "none";
+  }
+};
